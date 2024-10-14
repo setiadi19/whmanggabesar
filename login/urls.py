90959575cb62507b8f1643f90eb752273756e562
setiadi19 <setiaditeguh19@gmail.com>
2024-10-14 23:27:36 +0700

    Remove all files and folders

--- login/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-
-urlpatterns = [
-    path('', views.user_login, name="login"),  # Login URL
-    path('logout/', views.user_logout, name="logout"),  # Logout URL
-]
--- mysite/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('home.urls')),  # Mengarahkan ke app home
-    path('login/', include('login.urls')),  # Untuk halaman login dan logout
-    path('dashboard/', include('dashboard.urls')),  # Untuk halaman dashboard
-]
