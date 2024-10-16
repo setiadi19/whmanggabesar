b9b47620dcebedbc177a0ac5cc080b0b5fc51328
setiadi19 <155706624+setiadi19@users.noreply.github.com>
2024-10-17 00:54:07 +0700

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,8 @@
+
 from django.urls import path
-from . import views
+from . import views  # Mengimpor semua dari views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),  # URL untuk dashboard
+    path('send_notification/', views.send_notification, name='send_notification'),  # Mengakses send_notification dari views
 ]
-
